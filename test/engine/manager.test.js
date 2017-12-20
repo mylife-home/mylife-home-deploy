ec53e95725481076982703926b338e5aa0d047b0
vincent-tr <vincent@stoic.com>
2017-12-20 21:16:34 +0100

    manager tests

--- test/engine/manager.test.js
+++ test/engine/manager.test.js
@@ -45,7 +45,23 @@ async function managerScope(cb) {
   return { result, events : me.events };
 }
 
-// TODO: scope manager ops and construction/close
+async function waitTaskEnd(manager, runId) {
+  if(manager.getRun(runId).status === 'ended') {
+    return;
+  }
+
+  return new Promise(resolve => {
+    const listener = endRunId => {
+      if(endRunId !== runId) {
+        return;
+      }
+
+      manager.removeListener('run-end', listener);
+      return resolve();
+    };
+    manager.on('run-end', listener);
+  });
+}
 
 describe('Manager', () => {
 
@@ -107,15 +123,79 @@ describe('Manager', () => {
   });
 
   it('Should execute a simple recipe', async () => {
-    const manager = new Manager();
-    manager.createRecipe('recipe', [
-      { type: 'task', name: 'variables-set', parameters: { name: 'variable1', value: 'value1' } },
-      { type: 'task', name: 'variables-set', parameters: { name: 'variable2', value: 'value2' } }
-    ]);
+    const { result, events } = await managerScope(async manager => {
+      manager.createRecipe('recipe', [
+        { type: 'task', name: 'variables-set', parameters: { name: 'variable1', value: 'value1' } },
+        { type: 'task', name: 'variables-set', parameters: { name: 'variable2', value: 'value2' } }
+      ]);
 
-    manager.startRecipe('recipe');
-    // wait for end, then show run
-    // TODO
+      const runId = manager.startRecipe('recipe');
+
+      await waitTaskEnd(manager, runId);
+
+      return {
+        runs : manager.listRuns(),
+        run  : manager.getRun(runId)
+      };
+    });
+
+    expect(result).to.deep.equal({
+      runs : [ 1 ],
+      run  : {
+        id         : 1,
+        recipeName : 'recipe',
+        status     : 'ended',
+        logs       : [
+          { severity : 'info', category : 'recipe',        message : 'begin \'recipe\''   },
+          { severity : 'info', category : 'variables:set', message : 'variable1 = value1' },
+          { severity : 'info', category : 'variables:set', message : 'variable2 = value2' },
+          { severity : 'info', category : 'recipe',        message : 'end \'recipe\''     }
+        ]
+      }
+    });
+
+    expect(events).to.deep.equal([
+      { name: 'recipe-created', args: [ 'recipe' ] },
+      { name: 'run-created',    args: [ 1, 'recipe' ] },
+      { name: 'run-begin',      args: [ 1 ] },
+      { name: 'run-log',        args: [ 1, { severity: 'info', category: 'recipe',        message: 'begin \'recipe\''   } ] },
+      { name: 'run-log',        args: [ 1, { severity: 'info', category: 'variables:set', message: 'variable1 = value1' } ] },
+      { name: 'run-log',        args: [ 1, { severity: 'info', category: 'variables:set', message: 'variable2 = value2' } ] },
+      { name: 'run-log',        args: [ 1, { severity: 'info', category: 'recipe',        message: 'end \'recipe\''     } ] },
+      { name: 'run-end',        args: [ 1 ] }
+    ]);
+  });
+
+  it('Should execute a recipe with real async tasks', async () => {
+    const { result } = await managerScope(async manager => {
+      const source = '/Users/vincent/Downloads/rpi-devel-base.tar.gz';
+      manager.createRecipe('recipe', [
+        { type: 'task', name: 'image-import', parameters: { archiveName : source, rootPath : 'mmcblk0p1' } },
+        { type: 'task', name: 'config-init', parameters: { } },
+        { type: 'task', name: 'image-cache', parameters: { } }
+      ]);
+
+      const runId = manager.startRecipe('recipe');
+
+      await waitTaskEnd(manager, runId);
+
+      return manager.getRun(runId);
+    });
+
+    result.logs = result.logs.filter(it => it.severity !== 'debug');
+
+    expect(result).to.deep.equal({
+      id         : 1,
+      recipeName : 'recipe',
+      status     : 'ended',
+      logs       : [
+        { severity : 'info', category : 'recipe',       message: 'begin \'recipe\''                                                                                   },
+        { severity : 'info', category : 'image:import', message: 'import \'/Users/vincent/Downloads/rpi-devel-base.tar.gz\' using root path \'mmcblk0p1\' into image' },
+        { severity : 'info', category : 'config:init',  message: 'extract config from image file \'rpi-devel.apkovl.tar.gz\''                                         },
+        { severity : 'info', category : 'image:cache',  message: 'setup image cache'                                                                                  },
+        { severity : 'info', category : 'recipe',       message: 'end \'recipe\''                                                                                     }
+      ]
+    });
   });
 });
 
