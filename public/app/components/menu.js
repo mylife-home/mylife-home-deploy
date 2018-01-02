8e66cd214df1e969a6be480981f62ec1d0d48a12
vincent-tr <vincent@stoic.com>
2018-01-02 10:29:56 +0100

    display real menu

--- public/app/components/menu.js
+++ public/app/components/menu.js
@@ -1,9 +1,10 @@
 'use strict';
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Menu, Icon } from 'semantic-ui-react';
 
-const AppMenu = () => (
+const AppMenu = ({ recipes, runs }) => (
   <Menu icon='labeled' size='tiny' vertical fluid fixed='left'>
     <Menu.Item name='recipes'>
       <Menu.Header align='left' style={{ color: 'rgba(0, 0, 0, 0.5)' }}>
@@ -11,50 +12,32 @@ const AppMenu = () => (
         Recipes
       </Menu.Header>
       <Menu.Menu>
-        <Menu.Item name='pinned1'>
-          <Menu.Header align='left' style={{ marginLeft: '1em' }}>
-            <Icon name='file text outline' />
-            build-all
-          </Menu.Header>
-        </Menu.Item>
-        <Menu.Item name='pinned2' style={{ marginLeft: '1em' }}>
-          <Menu.Header align='left'>
-            <Icon name='file text outline' />
-            install-rpi2-home-epanel1
-          </Menu.Header>
-        </Menu.Item>
-        <Menu.Item name='pinned3' style={{ marginLeft: '1em' }}>
-          <Menu.Header align='left'>
-            <Icon name='file text outline' />
-            install-rpi2-home-garden2
-          </Menu.Header>
-        </Menu.Item>
+        {recipes.map(recipe =>
+          <Menu.Item key={recipe.name} name={recipe.name}>
+            <Menu.Header align='left' style={{ marginLeft: '1em' }}>
+              <Icon name='file text outline' />
+              {recipe.name}
+            </Menu.Header>
+          </Menu.Item>
+        )}
       </Menu.Menu>
     </Menu.Item>
     <Menu.Item name='flows'>
       <Menu.Header align='left' style={{ color: 'rgba(0, 0, 0, 0.5)' }}>
         <Icon name='tasks' />
-        Flows
+        Runs
       </Menu.Header>
       <Menu.Menu>
-        <Menu.Item name='flow1'>
-          <Menu.Header align='left' style={{ marginLeft: '1em' }}>
-            <Icon name='play' />
-            #1 build-all
-          </Menu.Header>
-        </Menu.Item>
-        <Menu.Item name='flow2'>
-          <Menu.Header align='left' style={{ marginLeft: '1em' }}>
-            <Icon name='checkmark' />
-            #2 build-all
-          </Menu.Header>
-        </Menu.Item>
-        <Menu.Item name='flow3'>
-          <Menu.Header align='left' style={{ marginLeft: '1em' }}>
-            <Icon name='remove' />
-            #3 build-all
-          </Menu.Header>
-        </Menu.Item>
+        {runs.map(run =>
+          <Menu.Item key={run.id} name={run.id}>
+            <Menu.Header align='left' style={{ marginLeft: '1em' }}>
+              <Icon name='play' /> { /* running */ }
+              <Icon name='checkmark' /> { /* success */ }
+              <Icon name='remove' /> { /* error */ }
+              {run.id}
+            </Menu.Header>
+          </Menu.Item>
+        )}
       </Menu.Menu>
     </Menu.Item>
     <Menu.Item name='files'>
@@ -65,5 +48,10 @@ const AppMenu = () => (
     </Menu.Item>
   </Menu>
 );
+
+AppMenu.propTypes = {
+  recipes : PropTypes.object,
+  runs    : PropTypes.object,
+};
 
 export default AppMenu;
