f2f49f79132c1d26d7471bacf761f2f80e6169f2
vincent-tr <vincent@stoic.com>
2018-01-02 14:59:02 +0100

    cosmetic

--- public/app/containers/menu-container.js
+++ public/app/containers/menu-container.js
@@ -2,9 +2,9 @@
 
 import { connect } from 'react-redux';
 
-import Menu                         from '../components/menu';
-import { getPinnedRecipes }         from '../selectors/recipes';
-import { getRuns }                  from '../selectors/runs';
+import Menu                 from '../components/menu';
+import { getPinnedRecipes } from '../selectors/recipes';
+import { getRuns }          from '../selectors/runs';
 
 const mapStateToProps = state => ({
   recipes : getPinnedRecipes(state),
