d6656ff0cdf47444bdf689a24f62114f2d2a1439
vincent-tr <vincent@stoic.com>
2018-01-19 10:45:43 +0100

    ssh server readdir

--- test/engine/ssh-server.js
+++ test/engine/ssh-server.js
@@ -1,6 +1,41 @@
 'use strict';
 
 const { Server, SFTP_STATUS_CODE, SFTP_OPEN_MODE } = require('ssh2');
+const vfs = require('../../lib/engine/vfs');
+
+const S_IFMT  = 0o0170000; // bit mask for the file type bit fields
+const S_IFREG = 0o0100000; // regular file
+const S_IFDIR = 0o0040000; // directory
+const S_IFLNK = 0o0120000; // symbolic link
+const S_IRUSR = 0o00400;   // owner has read permission
+const S_IWUSR = 0o00200;   // owner has write permission
+const S_IXUSR = 0o00100;   // owner has execute permission
+const S_IRGRP = 0o00040;   // group has read permission
+const S_IWGRP = 0o00020;   // group has write permission
+const S_IXGRP = 0o00010;   // group has execute permission
+const S_IROTH = 0o00004;   // others have read permission
+const S_IWOTH = 0o00002;   // others have write permission
+const S_IXOTH = 0o00001;   // others have execute permission
+
+const fileTypeLongname = new Map([
+  [ S_IFREG , '-' ],
+  [ S_IFDIR , 'd' ],
+  [ S_IFLNK , 'l' ]
+]);
+
+const permtoLongname = [
+  { num : S_IRUSR, value : 'r' },
+  { num : S_IWUSR, value : 'w' },
+  { num : S_IXUSR, value : 'x' },
+  { num : S_IRGRP, value : 'r' },
+  { num : S_IWGRP, value : 'w' },
+  { num : S_IXGRP, value : 'x' },
+  { num : S_IROTH, value : 'r' },
+  { num : S_IWOTH, value : 'w' },
+  { num : S_IXOTH, value : 'x' }
+];
+
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
 
 const sftpEvents = [
   'OPEN',
@@ -38,12 +73,12 @@ class HandleTable {
   }
 
   _bufferToInt(buffer) {
-    return buffer.readUint32LE(0);
+    return buffer.readUInt32LE(0);
   }
 
   _bufferFromInt(value) {
     const buffer = Buffer.allocUnsafe(4);
-    buffer.writeUint32LE(value, 0);
+    buffer.writeUInt32LE(value, 0);
     return buffer;
   }
 
@@ -60,7 +95,7 @@ class HandleTable {
 
   delete(handle) {
     const id = this._bufferToInt(handle);
-    this.map.delete(id);
+    return this.map.delete(id);
   }
 }
 
@@ -69,7 +104,58 @@ class OpenedFile {
 }
 
 class OpenedDirectory {
+  constructor(node) {
+    this.node = node;
+    this.eof = false;
+  }
 
+  content() {
+    if(this.eof) {
+      return [];
+    }
+
+    this.eof = true;
+
+    return this.node.list().map(node => {
+
+      let mode = node.mode;
+      if(node instanceof vfs.File)      { mode += S_IFREG; }
+      if(node instanceof vfs.Directory) { mode += S_IFDIR; }
+      if(node instanceof vfs.Symlink)   { mode += S_IFLNK; }
+
+      let size = 0;
+      if(node instanceof vfs.File)    { size = node.content.length; }
+      if(node instanceof vfs.Symlink) { size = node.target.length; }
+
+      const { uid, gid, atime, mtime } = node;
+      const date = new Date(mtime);
+
+      let longname = fileTypeLongname.get(mode & S_IFMT) || ' ';
+      for(const item of permtoLongname) {
+        longname += (mode & item.num) ? item.value : '-';
+      }
+
+      longname += ' 1';
+      longname += ' ' + uid.toString().padEnd(8);
+      longname += ' ' + gid.toString().padEnd(8);
+      longname += ' ' + size.toString().padStart(12);
+      longname += ' ' + monthNames[date.getMonth()];
+      longname += ' ' + date.getDate().toString().padStart(2);
+      longname += ' ' + date.getHours().toString().padStart(2, '0');
+      longname += ':' + date.getMinutes().toString().padStart(2, '0');
+      longname += ' ' + node.name;
+
+      if(node instanceof vfs.Symlink) {
+        longname += ' -> ' + node.target;
+      }
+
+      return {
+        filename : node.name,
+        attrs    : { mode, size, uid, gid, atime, mtime },
+        longname
+      };
+    });
+  }
 }
 
 class RequestContext {
@@ -141,15 +227,36 @@ class SFTPSession {
   }
 
   async close(ctx, handle) {
-    await ctx.status(SFTP_STATUS_CODE.OP_UNSUPPORTED);
+    const ret = this.handleTable.delete(handle);
+    await ctx.status(ret ? SFTP_STATUS_CODE.OK : SFTP_STATUS_CODE.FAILURE);
   }
 
   async opendir(ctx, path) {
-    await ctx.status(SFTP_STATUS_CODE.OP_UNSUPPORTED);
+    const node = vfs.path(this.rootfs, path.split('/').filter(n => n), true);
+    if(!node) {
+      return await ctx.status(SFTP_STATUS_CODE.NO_SUCH_FILE);
+    }
+
+    if(!(node instanceof vfs.Directory)) {
+      return await ctx.status(SFTP_STATUS_CODE.NO_SUCH_FILE);
+    }
+
+    const openedDirectory = new OpenedDirectory(node);
+    const handle = this.handleTable.create(openedDirectory);
+    await ctx.handle(handle);
   }
 
   async readdir(ctx, handle) {
-    await ctx.status(SFTP_STATUS_CODE.OP_UNSUPPORTED);
+    const openedDirectory = this.handleTable.target(handle);
+    if(!openedDirectory) {
+      return await ctx.status(SFTP_STATUS_CODE.FAILURE);
+    }
+
+    if(openedDirectory.eof) {
+      return await ctx.status(SFTP_STATUS_CODE.EOF);
+    }
+
+    await ctx.name(openedDirectory.content());
   }
 
   async lstat(ctx, path) {
--- test/engine/ssh.test.js
+++ test/engine/ssh.test.js
@@ -7,81 +7,112 @@ const vfs           = require('../../lib/engine/vfs');
 const { SSHClient } = require('../../lib/engine/ssh');
 const { SSHServer } = require('./ssh-server');
 
-describe('SSH Client', () => {
+describe('SSH', () => {
 
-  describe.skip('In real world', () => {
+  describe('Basic execution', () => {
 
-    const rootList = [ '.modloop', 'bin', 'dev', 'etc', 'home', 'lib', 'media', 'mnt', 'proc', 'root', 'run', 'sbin', 'srv', 'sys', 'tmp', 'usr', 'var' ];
+    describe.skip('Client to real server', () => {
 
-    async function runRealClientTest(tester) {
-      const privateKey = fs.readFileSync('/Users/vincent/Downloads/id_rsa');
-      const client = new SSHClient();
-      await client.connect({ host : 'rpi-devel', username : 'root', privateKey });
-      try {
-        await tester(client);
-      } finally {
-        client.end();
+      const rootList = [ '.modloop', 'bin', 'dev', 'etc', 'home', 'lib', 'media', 'mnt', 'proc', 'root', 'run', 'sbin', 'srv', 'sys', 'tmp', 'usr', 'var' ];
+
+      async function runClientRealServerTest(tester) {
+        const privateKey = fs.readFileSync('/Users/vincent/Downloads/id_rsa');
+        const client = new SSHClient();
+        await client.connect({ host : 'rpi-devel', username : 'root', privateKey });
+        try {
+          await tester(client);
+        } finally {
+          client.end();
+        }
       }
-    }
 
-    it('Should properly execute command on remote host in real world', async () => runRealClientTest(async client => {
-      expect(await client.exec('ls -a /')).to.equal([ '.', '..', ...rootList ].map(it => `${it}\n`).join(''));
-    }));
+      it('Should properly execute command on remote host in real world', async () => runClientRealServerTest(async client => {
+        expect(await client.exec('ls -a /')).to.equal([ '.', '..', ...rootList ].map(it => `${it}\n`).join(''));
+      }));
 
-    it('Should properly read sftp directory on remote host in real world', async () => runRealClientTest(async client => {
-      const list = await client.sftpReaddir('/');
-      expect(list.map(it => it.filename).sort()).to.deep.equal(rootList);
-    }));
+      it('Should properly read sftp directory on remote host in real world', async () => runClientRealServerTest(async client => {
+        const list = await client.sftpReaddir('/');
+        expect(list.map(it => it.filename).sort()).to.deep.equal(rootList);
+      }));
 
-    it('Should combine exec and sftp in same session in real world', async () => runRealClientTest(async client => {
-      await client.exec('ls -a /');
-      await client.sftpReaddir('/');
-      await client.exec('ls -a /');
-      await client.sftpReaddir('/');
-    }));
+      it('Should combine exec and sftp in same session in real world', async () => runClientRealServerTest(async client => {
+        await client.exec('ls -a /');
+        await client.sftpReaddir('/');
+        await client.exec('ls -a /');
+        await client.sftpReaddir('/');
+      }));
+    });
+
+    describe('Client to mocked server', () => {
+      async function runClientMockedServerTest(rootfs, cmdhandler, tester) {
+        const port = 8822;
+        const server = new SSHServer({ port, rootfs, cmdhandler, hostKeys : [ fs.readFileSync(path.resolve(__dirname, 'content/id_rsa')) ] });
+        const client = new SSHClient();
+        await client.connect({ host : 'localhost', port, username : 'root', password : 'nothing' });
+        try {
+          await tester(client);
+        } finally {
+          client.end();
+          server.close();
+        }
+      }
+
+      const command = 'MyCmd';
+      const commandResult = 'MyResult';
+
+      function cmdHandler(cmd) {
+        if(command !== cmd) {
+          throw new Error('Unknown command');
+        }
+        return commandResult;
+      }
+
+      it('Should properly execute command on mocked server', async () => await runClientMockedServerTest(new vfs.Directory(), cmdHandler, async (client) => {
+        expect(await client.exec(command)).to.equal(commandResult);
+      }));
+
+      it('Should fail to execute wrong command on mocked server', async () => await runClientMockedServerTest(new vfs.Directory(), cmdHandler, async (client) => {
+        let err;
+        try {
+          await client.exec('wrong command');
+        } catch(exc) {
+          err = exc;
+        }
+        expect(err).to.match(/Error: Command has error output : 'Unknown command'/);
+      }));
+
+      it('Should properly use sftp to read directory on moched server', async () => {
+        const rootfs = new vfs.Directory();
+        const attrs = { uid : 5, gid : 6, atime : new Date(2000, 0, 1, 10, 30), mtime : new Date(2010, 5, 10, 10, 30) };
+
+        rootfs.add(new vfs.Directory({ name : 'dir',   mode : 0o755, ...attrs }));
+        rootfs.add(new vfs.File(     { name : 'file',  mode : 0o644, ...attrs, content : Buffer.alloc(10) }));
+        rootfs.add(new vfs.Symlink(  { name : 'slink', mode : 0o777, ...attrs, target : './file' }));
+
+        const eattrs = { ...attrs, atime : attrs.atime.valueOf() / 1000, mtime : attrs.mtime.valueOf() / 1000 };
+
+        const expected = [{
+          attrs    : { ...eattrs, size : 0, mode : 0o040755, permissions : 0o040755 },
+          filename : 'dir',
+          longname : 'drwxr-xr-x 1 5        6                   0 Jun 10 10:30 dir'
+        }, {
+          attrs    : { ...eattrs, size : 10, mode : 0o100644, permissions : 0o100644 },
+          filename : 'file',
+          longname : '-rw-r--r-- 1 5        6                  10 Jun 10 10:30 file'
+        }, {
+          attrs    : { ...eattrs, size : './file'.length, mode : 0o120777, permissions : 0o120777 },
+          filename : 'slink',
+          longname : 'lrwxrwxrwx 1 5        6                   6 Jun 10 10:30 slink -> ./file'
+        }];
+        await runClientMockedServerTest(rootfs, cmdHandler, async (client) => {
+          const result = await client.sftpReaddir('/');
+          // need clone because result contains Stats objects
+          expect(JSON.parse(JSON.stringify(result))).to.deep.equal(expected);
+        });
+      });
+    });
   });
 
-  describe('Using mocked server', () => {
+  //describe('SFTP manipulations')
 
-    async function runClientTest(rootfs, cmdhandler, tester) {
-      const port = 8822;
-      const server = new SSHServer({ port, rootfs, cmdhandler, hostKeys : [ fs.readFileSync(path.resolve(__dirname, 'content/id_rsa')) ] });
-      const client = new SSHClient();
-      await client.connect({ host : 'localhost', port, username : 'root', password : 'nothing' });
-      try {
-        await tester(client);
-      } finally {
-        client.end();
-        server.close();
-      }
-    }
-
-    const command = 'MyCmd';
-    const commandResult = 'MyResult';
-
-    function cmdHandler(cmd) {
-      if(command !== cmd) {
-        throw new Error('Unknown command');
-      }
-      return commandResult;
-    }
-
-    it('Should properly execute command on mocked server', async () => await runClientTest(new vfs.Directory(), cmdHandler, async (client) => {
-      expect(await client.exec(command)).to.equal(commandResult);
-    }));
-
-    it('Should fail to execute wrong command on mocked server', async () => await runClientTest(new vfs.Directory(), cmdHandler, async (client) => {
-      let err;
-      try {
-        await client.exec('wrong command');
-      } catch(exc) {
-        err = exc;
-      }
-      expect(err).to.match(/Error: Command has error output : 'Unknown command'/);
-    }));
-
-    it('Should properly access sftp on moched server', async () => await runClientTest(new vfs.Directory(), cmdHandler, async (client) => {
-      expect(await client.sftpReaddir('/')).to.equal([]);
-    }));
-  });
 });
